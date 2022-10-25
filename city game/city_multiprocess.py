9314e2c419754e14790ad7eb6d23620cfaa47cfc
donutyeon <y606800@gmail.com>
2022-10-25 18:47:34 +0100

    organized pictures in separate folder

--- city game/city_multiprocess.py
+++ city game/city_multiprocess.py
@@ -6,7 +6,7 @@ import threading
 WHITE = (255, 255, 255)
 import ctypes
 import random
-buildings=[pygame.image.load("building1.png"),pygame.image.load("building2.png"),pygame.image.load("building3.png"),pygame.image.load("building4.png")]
+buildings=[pygame.image.load("game_sprites\_building1.png"),pygame.image.load("game_sprites\_building2.png"),pygame.image.load("game_sprites\_building3.png"),pygame.image.load("game_sprites\_building4.png")]
 class Car(pygame.sprite.Sprite):
     #This class represents a car. It derives from the "Sprite" class in Pygame.
 
@@ -26,7 +26,7 @@ class Car(pygame.sprite.Sprite):
         #pygame.draw.rect(self.image, self.color, [0, 0, self.width, self.height])
 
         # Instead we could load a proper picture of a car...
-        self.image = pygame.image.load("car_up.png")
+        self.image = pygame.image.load("game_sprites\car_up.png")
 
         # Fetch the rectangle object that has the dimensions of the image.
         self.rect = self.image.get_rect()
@@ -38,7 +38,7 @@ class Car(pygame.sprite.Sprite):
     ## moving functions
     def droite(self):
         #change the sprite
-        self.image = pygame.image.load("car_right.png")
+        self.image = pygame.image.load("game_sprites\car_right.png")
         sleep(0.5)
         with self.lock:
             self.rect.x += 32
@@ -47,7 +47,7 @@ class Car(pygame.sprite.Sprite):
             #checking collision with a wall
             if self.rect.colliderect(wall.rect):
                 self.rect.right = wall.rect.left
-                self.image = pygame.transform.scale(pygame.image.load("explosion.png").convert_alpha(), (int(64 * 0.5), int(64 * 0.5)))
+                self.image = pygame.transform.scale(pygame.image.load("game_sprites\explosion.png").convert_alpha(), (int(64 * 0.5), int(64 * 0.5)))
                 self.collided=True
                 ctypes.windll.user32.MessageBoxW(0, "you ran into a wall, game over.", "Game Over", 0)
         for light in self.lights:
@@ -59,7 +59,7 @@ class Car(pygame.sprite.Sprite):
   
     #same thing as right but with a different direction
     def gauche(self):
-        self.image = pygame.image.load("car_left.png")
+        self.image = pygame.image.load("game_sprites\car_left.png")
         sleep(0.5)
         with self.lock: #we change the coordinates of the player, one tile is 32x32 px, so we move by 32px in the desired direction
             self.rect.x -= 32
@@ -67,7 +67,7 @@ class Car(pygame.sprite.Sprite):
         for wall in self.walls: #we check everytime if we have collided with a wall during our move
             if self.rect.colliderect(wall.rect):
                 self.rect.left = wall.rect.right
-                self.image = pygame.transform.scale(pygame.image.load("explosion.png").convert_alpha(), (int(64 * 0.5), int(64 * 0.5)))
+                self.image = pygame.transform.scale(pygame.image.load("game_sprites\explosion.png").convert_alpha(), (int(64 * 0.5), int(64 * 0.5)))
                 self.collided=True          
                 ctypes.windll.user32.MessageBoxW(0, "you ran into a wall, game over.", "Game Over", 0) #if we did collide, then it's game over
         for light in self.lights: #checking if we ran a red light
@@ -78,7 +78,7 @@ class Car(pygame.sprite.Sprite):
                 
 
     def avancer(self):
-        self.image = pygame.image.load("car_up.png")
+        self.image = pygame.image.load("game_sprites\car_up.png")
         sleep(0.5)
         with self.lock:
             self.rect.y -= 32
@@ -86,7 +86,7 @@ class Car(pygame.sprite.Sprite):
         for wall in self.walls:
             if self.rect.colliderect(wall.rect):
                 self.rect.top = wall.rect.bottom
-                self.image = pygame.transform.scale(pygame.image.load("explosion.png").convert_alpha(), (int(64 * 0.5), int(64 * 0.5)))
+                self.image = pygame.transform.scale(pygame.image.load("game_sprites\explosion.png").convert_alpha(), (int(64 * 0.5), int(64 * 0.5)))
                 self.collided=True              
                 ctypes.windll.user32.MessageBoxW(0, "you ran into a wall, game over.", "Game Over", 0)
         for light in self.lights:
@@ -95,7 +95,7 @@ class Car(pygame.sprite.Sprite):
                 self.ran=True
 
     def reculer(self):
-        self.image = pygame.image.load("car_down.png")
+        self.image = pygame.image.load("game_sprites\car_down.png")
         sleep(0.5)
         with self.lock:
             self.rect.y += 32
@@ -103,7 +103,7 @@ class Car(pygame.sprite.Sprite):
         for wall in self.walls:
             if self.rect.colliderect(wall.rect):
                 self.rect.bottom = wall.rect.top
-                self.image = pygame.transform.scale(pygame.image.load("explosion.png").convert_alpha(), (int(64 * 0.5), int(64 * 0.5)))
+                self.image = pygame.transform.scale(pygame.image.load("game_sprites\explosion.png").convert_alpha(), (int(64 * 0.5), int(64 * 0.5)))
                 self.collided=True                
                 ctypes.windll.user32.MessageBoxW(0, "you ran into a wall, game over.", "Game Over", 0)
         for light in self.lights:
@@ -130,11 +130,11 @@ class TrafficLight(object):
         self.y=int(pos[1])
         self.rect = pygame.Rect(pos[0], pos[1], 32, 32)
         if color=="RED":
-            self.image=pygame.image.load("lights_red.png")
+            self.image=pygame.image.load("game_sprites\lights_red.png")
         if color=="YELLOW":
-            self.image=pygame.image.load("lights_yellow.png")
+            self.image=pygame.image.load("game_sprites\lights_yellow.png")
         if color=="GREEN":
-            self.image=pygame.image.load("lights_green.png")
+            self.image=pygame.image.load("game_sprites\lights_green.png")
 
 # the tiles is basically the floor in order to have road sprites
 class Tiles(object):
@@ -209,7 +209,7 @@ class city_game:
                     Wall((x, y), self.walls,buildings[rand])
                 if col == "E":
                     self.end_rect = pygame.Rect(x, y, 32, 32)
-                    self.end_image= pygame.image.load("end.png")
+                    self.end_image= pygame.image.load("game_sprites\end.png")
                 if col == "P":
                     self.logic.player.rect.x = x
                     self.logic.player.rect.y = y
@@ -220,9 +220,9 @@ class city_game:
                 if col == "Y":
                     TrafficLight((x, y),self.lights ,"YELLOW")
                 if col==' ':
-                    Tiles((x,y),self.tiles,"route_straight.png")
+                    Tiles((x,y),self.tiles,"game_sprites\_route_straight.png")
                 if col=='-':
-                    Tiles((x,y),self.tiles,"route_side.png")
+                    Tiles((x,y),self.tiles,"game_sprites\_route_side.png")
                 x += 32
             y += 32
             x = 0
