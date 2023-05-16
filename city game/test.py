516277ed3cfff2cf802153df9b0255fce2648611
donutyeon <y606800@gmail.com>
2023-05-16 23:00:39 +0100

    updated examples

--- city game/test.py
+++ city game/test.py
@@ -1,14 +1,15 @@
 from city_lab import city_game
 game=city_game(1)
+
+#Distance at the start of the game
 print('Euclidean distance: ',game.euclidean_distance())
 print('Manhattan distance: ',game.manhattan_distance())
 print('XY distance: ',game.XY_distance())
-#run this to test if Red Light
-#game.afficher()
-# while(game.isWall() == False):
-#     game.avancer()
-# game.droite()
 
+#run this to show the map 
+#game.afficher()
+
+#Run this to win the game
 for i in range (3):
     game.avancer()
 game.gauche()
@@ -38,28 +39,29 @@ if game.isRedLight():
 
 game.start()
 
-#run this to run a red light
-for i in range (4):
-    game.avancer()
-for i in range (7):
-    game.gauche()
-for i in range (3):
-    game.avancer()
-for i in range (3):
-    game.gauche()
-
-#run this part to win the game
-for i in range (3):
-    game.avancer()
-for i in range (11):
-    game.gauche()
-for i in range (8):
-    game.avancer()
-#game.start()
-
 #run this to collide into a wall
 for i in range (4):
     game.avancer()
+game.start()
 
-#show the map without the losing messagebox
-game.afficher()
+#Run this to lose the game (not getting to the destination)
+while game.isWall()==False:
+    game.avancer()
+game.droite()
+for i in range (2):
+    game.avancer()
+game.start()
+
+#Run this to run a red light
+for i in range (3):
+    game.avancer()
+game.gauche()
+for i in range (7):
+    game.avancer()
+game.droite()
+for i in range (3):
+    game.avancer()
+game.gauche()
+for i in range (3):
+    game.avancer()
+game.start()
